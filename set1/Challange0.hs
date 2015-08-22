478fb88fe44bbd3ce5c8a532fceafcebb15aa9f1
Andrew Chen <andrew.chuanye.chen@gmail.com>
2015-08-22 13:10:03 +1200

    Move test code into challange code

--- set1/Challange0.hs
+++
@@ -1,9 +0,0 @@
-import Base64
-import Hex
-
-import Test.QuickCheck
-
-main = do
-    putStrLn "===== Set1 ====="
-    quickCheck $ \v -> base64Decode (base64Encode v) == v
-    quickCheck $ \v -> hexDecode (hexEncode v) == v
--- set1/Challange1.hs
+++ set1/Challange1.hs
@@ -4,5 +4,7 @@ import Hex
 
 main = do
     putStrLn "=== Challange1 ==="
+    quickCheck $ \v -> base64Decode (base64Encode v) === v
+    quickCheck $ \v -> hexDecode (hexEncode v) === v
     putStrLn $ base64Encode $ hexDecode
            "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
